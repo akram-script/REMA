b019a7e6a9d4aa7ea6b96dbf8ccafb08a4d57c30
akram-script <akramreda17@gmail.com>
2023-06-24 18:40:24 +0100

    detail view

--- FrontEnd/src/app/services/housing.service.ts
+++ FrontEnd/src/app/services/housing.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-prototype-builtins */
 import { HttpClient } from '@angular/common/http';
@@ -11,30 +12,90 @@ import { property } from '../Model/property';
 })
 export class HousingService {
 
-
   constructor(private http : HttpClient) { }
 
-  addProperty(property: property) {
-   localStorage.setItem('newProp',JSON.stringify(property)) ;
+
+  getProperty(propertyId: number) :Observable<property>{
+    return this.http.get('data/properties.json').pipe(
+      map(data => {
+        const propertiesArray: Array<IProperty> = [];
+        const localProps = JSON.parse(localStorage.getItem('newProp'));
+        if (localProps) {
+          console.log(localProps);
+          this.pushToAllProps(localProps, propertiesArray);
+        }
+        this.pushToAllProps(data, propertiesArray);
+        const property = propertiesArray.find(property => property.ID === propertyId);
+        return property;
+      }),
+      tap(response => this.log(response)),
+      catchError(error => this.handleError(error, undefined))
+    );
   }
 
 
-  GetHouses(sellRent : number) : Observable<IProperty[] | undefined> {
+  addProperty(property: property) {
+    let newProp = [property];
+
+    const storedProp = localStorage.getItem('newProp');
+    if (storedProp) {
+      try {
+        const parsedProp = JSON.parse(storedProp);
+        if (Array.isArray(parsedProp)) {
+          newProp = [property, ...parsedProp];
+        } else {
+          newProp = [property, parsedProp];
+        }
+      } catch (error) {
+        console.error('Error parsing storedProp:', error);
+      }
+    }
+    localStorage.setItem('newProp', JSON.stringify(newProp));
+  }
+
+
+  GetHouses(sellRent? : number) : Observable<IProperty[] | undefined> {
     return this.http.get('data/properties.json').pipe(
       map( data => {
             const propertiesArray : Array<IProperty> = [] ;
-            console.log(sellRent) ;
-            for (const id in data) {
-              if(data.hasOwnProperty(id) && data[id as keyof object]['SellRent'] === sellRent){
-                propertiesArray.push(data[id as keyof object]);
-              }
+            const localProps = JSON.parse(localStorage.getItem('newProp'));
+            if(localProps) {
+              console.log(localProps);
+              this.pushToProps(localProps, sellRent, propertiesArray);
             }
+            this.pushToProps(data, sellRent, propertiesArray);
             return propertiesArray ;
       }) ,
       tap((Response) => this.log(Response)) ,
       catchError((error)=> this.handleError(error , undefined))
     ) ;
   }
+  private pushToProps(localProps: any, sellRent: number, propertiesArray: IProperty[]) {
+    for (const id in localProps) {
+      if (localProps.hasOwnProperty(id) && localProps[id as keyof object]['SellRent'] === sellRent) {
+        propertiesArray.push(localProps[id as keyof object]);
+      }
+    }
+  }
+  private pushToAllProps(localProps: any, propertiesArray: IProperty[]) {
+    for (const id in localProps) {
+      if (localProps.hasOwnProperty(id) && localProps[id as keyof object]) {
+        propertiesArray.push(localProps[id as keyof object]);
+      }
+    }
+  }
+
+  newPropId() : number{
+    if(localStorage.getItem('PID')){
+      localStorage.setItem('PID',String(+localStorage.getItem('PID')+1));
+      return +localStorage.getItem('PID') ;
+    }else{
+      localStorage.setItem('PID','101');
+      return 101 ;
+    }
+
+  }
+
   private log(response : any){
     console.table(response);
    }

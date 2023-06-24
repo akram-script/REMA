b558de756a9cb89503b3a55353512883dfa3b45d
akram-script <akramreda17@gmail.com>
2023-06-24 14:52:40 +0100

    properties-details

--- FrontEnd/src/app/property/property-list/property-list.component.ts
+++ FrontEnd/src/app/property/property-list/property-list.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-inferrable-types */
 
+import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
+import { ActivatedRoute } from '@angular/router';
+import { IProperty } from 'src/app/Model/iproperty';
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
@@ -7,7 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  Properties : Array<IProperty> | undefined = [] ;
+  sellRent : number = 1 ;
+  /**
+   *
+   */
+  constructor(private route : ActivatedRoute,private service : HousingService) {}
+
   ngOnInit(): void {
-     console.log('list loaded');
+    if(this.route.snapshot.url.toString()){
+        this.sellRent = 2
+    }
+
+    this.service.GetHouses(this.sellRent).subscribe(properties => {
+      this.Properties = properties ;
+      const newproperty  = JSON.parse(localStorage.getItem('newProp')) ;
+      if(newproperty.SellRent == this.sellRent){
+        this.Properties = [newproperty , ...this.Properties]
+      }
+
+      }) ;
   }
 }

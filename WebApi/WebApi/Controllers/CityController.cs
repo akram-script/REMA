8a9f8a4865fe09d3f2a0e4dd0496cf07a8712b17
akram-script <akramreda17@gmail.com>
2023-07-01 18:21:57 +0100

    add

--- WebApi/WebApi/Controllers/CityController.cs
+++ WebApi/WebApi/Controllers/CityController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApi.Data;
+using WebApi.Models;
 
 namespace WebApi.Controllers
 {
@@ -20,6 +21,39 @@ namespace WebApi.Controllers
         {
             var Cities = await dc.Cities.ToListAsync();
             return  Ok(Cities);
+        }
+       
+        [HttpPost("add")]
+        [HttpPost("add/{CityName}")]
+        public async Task<IActionResult> AddCity(string CityName)
+        {
+            var city = new City()
+            {
+                Name = CityName,
+            };
+           await dc.Cities.AddAsync(city);
+           await dc.SaveChangesAsync();
+            return Ok(city);
+        }
+        [HttpPost("post")]
+        public async Task<IActionResult> AddCity(City city)
+        {
+            
+            await dc.Cities.AddAsync(city);
+            await dc.SaveChangesAsync();
+            return Ok(city);
+        }
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            var city = await dc.Cities.FindAsync(id);
+
+            if (city != null)
+            {
+                dc.Cities.Remove(city);
+                await dc.SaveChangesAsync();
+            }
+            return Ok(id);
         }
         [HttpGet("{id}")]
         public string Get(int id) { return "Atlanta"; }
